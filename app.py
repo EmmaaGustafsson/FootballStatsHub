8d07cd9c147bdc1f4ed6947e4efbfc650967c46d
Filip SW <filipsw1@gmail.com>
2026-01-29 09:43:14 +0100

    added streamlit app and updated requirements

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FootyStatsHub",
+    page_icon="⚽",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.title("⚽ FootballStatsHub")
+st.markdown("### Välkommen till din fotbollsstatistik-app!")
+st.success("Streamlit-servern körs!")
